f7db1edc95d29c250cab5ade3bca396fd13cd1f8
Denis J. Lopez G. <denislopezgrandez24@gmail.com>
2025-10-24 14:58:58 -0600

    Avance primer ejercicio

--- src/lab2p1_denis/lopez/Lab2P1_DenisLopez.java
+++ src/lab2p1_denis/lopez/Lab2P1_DenisLopez.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab2p1_denis.lopez;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,57 @@ public class Lab2P1_DenisLopez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        System.out.println("Menu");
+        System.out.println("1. Hola Mundo");
+        System.out.println("2. Protege tu Nota");
+        System.out.println("3. Sumatoria de cuadrados");
+        System.out.println("4. Salir");
+        System.out.println("Ingrese su opcion: ");
+        int opcion = leer.nextInt();
+        char respuesta = 's';
+        while (opcion!=4){
+            if (opcion==1){
+                System.out.println("Ingrese un numero mayor que 1 y menor que 100");
+                int numero = leer.nextInt();
+                int contador=0;
+                while (numero>1 && numero<100){
+                    if (contador<=numero){
+                    contador=contador+1;
+                    System.out.println(+contador);
+                    }
+                    if (contador%5==3){
+                        System.out.print("Hola");
+                    }
+                    if (contador%3==5){
+                        System.out.print("Mundo");
+                    }
+                    if (contador%5==3 && contador%3==5){
+                        System.out.println("Hola Mundo");
+                    }
+                }
+                System.out.println("Numero fuera de rango. Por favor, intente de nuevo.");  
+            }
+            else
+                if (opcion==2){
+                    
+                }
+            else
+                if (opcion==3){
+                    
+                }
+        }
+        System.out.println("Numero no valido");
+        System.out.println("Menu");
+        System.out.println("1. Hola Mundo");
+        System.out.println("2. Protege tu Nota");
+        System.out.println("3. Sumatoria de cuadrados");
+        System.out.println("4. Salir");
+        System.out.println("Ingrese su opcion: ");
+        opcion = leer.nextInt();
         // TODO code application logic here
+        
     }
     
 }
+
